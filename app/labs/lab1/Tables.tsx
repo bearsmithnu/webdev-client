241cc0f75f69a48e0cf49d28c5f769a9db8dbc03
Bear Smith <smith.wa@northeastern.edu>
2026-09-17 16:52:33 -0400

    add rows for quizzes 4-10

--- app/labs/lab1/Tables.tsx
+++ app/labs/lab1/Tables.tsx
@@ -30,11 +30,53 @@ export default function Tables() {
             <td align="center">2/17/21</td>
             <td align="right">95</td>
           </tr>
+          <tr>
+            <td>Q4</td>
+            <td align="center">Next.js</td>
+            <td align="center">2/19/21</td>
+            <td align="right">100</td>
+          </tr>
+          <tr>
+            <td>Q5</td>
+            <td align="center">React</td>
+            <td align="center">2/21/21</td>
+            <td align="right">95</td>
+          </tr>
+          <tr>
+            <td>Q6</td>
+            <td align="center">Zustand</td>
+            <td align="center">2/23/21</td>
+            <td align="right">97</td>
+          </tr>
+          <tr>
+            <td>Q7</td>
+            <td align="center">Express REST APIs</td>
+            <td align="center">2/25/21</td>
+            <td align="right">92</td>
+          </tr>
+          <tr>
+            <td>Q8</td>
+            <td align="center">MongoDB</td>
+            <td align="center">3/1/21</td>
+            <td align="right">97</td>
+          </tr>
+          <tr>
+            <td>Q9</td>
+            <td align="center">MySQL</td>
+            <td align="center">3/3/21</td>
+            <td align="right">100</td>
+          </tr>
+          <tr>
+            <td>Q10</td>
+            <td align="center">NoSQL</td>
+            <td align="center">3/5/21</td>
+            <td align="right">100</td>
+          </tr>
         </tbody>
         <tfoot>
           <tr>
             <td colSpan={3}>Average</td>
-            <td align="right">90</td>
+            <td align="right">95</td>
           </tr>
         </tfoot>
       </table>
